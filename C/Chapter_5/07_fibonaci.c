52a72175823cc7c9a56ad7b72cc5f2b6321bdd9d
Sravan <mnvsravan07@gmail.com>
2025-10-12 19:03:13 +0530

    gh

--- C/Chapter_5/07_fibonaci.c
+++ C/Chapter_5/07_fibonaci.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-int fibonaci(int );
-int fibonaci(int n) {
-    if (n == 0) {
-        return 0; // Base case: fib(0) = 0
-    } else if (n == 1 || n == 2) {
-        return n-1; // Base case: fib(1) = 1
-    }
-    return fibonaci(n - 1) + fibonaci(n - 2); // Recursive case
+
+int fibonacci(int n) {
+    if (n == 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
 int main() {
-    int number=7;
-    printf("The %dth Fibonacci number is %d\n", number, fibonaci(number));
+    int number = 7;
+    printf("The %dth Fibonacci number is %d\n", number, fibonacci(number));
     return 0;
 }
+
 
