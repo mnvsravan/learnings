af5a9f1f5fa9749e5187b33721687fb1048b5da7
mnvsravan <mnvsravan07@example.com>
2026-02-08 12:32:50 +0530

    worktree clean

---
+++ .vscode/Testing my pc/dfdhh.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a; 
+    char ch=a;
+    ch='c'; // Assigning character 'a' to variable ch
+    ch='\n'; // Using escape sequence for newline
+    printf("hey im god\n nice");
+
+    
+    return 0; // use \t for space
+}
---
+++ .vscode/Testing my pc/dfghgf.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    printf("hey im good\n nice");
+
+    
+    return 0; // use \t for space
+}
