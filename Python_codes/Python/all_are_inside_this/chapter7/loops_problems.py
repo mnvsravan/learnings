bd4cad8649dea0598953775d36065d407ca14bfa
mnvsravan <mnvsravan07@example.com>
2026-02-17 18:48:36 +0530

    changed

--- Python_codes/Python/all_are_inside_this/chapter7/loops_problems.py
+++ Python_codes/Python/all_are_inside_this/chapter7/loops_problems.py
@@ -104,3 +104,12 @@ for i in range(n8 - 2):
 
 # Bottom row
 print("*" * n8)
+
+
+#printing table in reverse order 
+
+table_of = int(input("Enter number: "))
+
+for i in range(1, 11):
+    print(f"{table_of} x {11-i} = {table_of * (11-i)}")
+    i=i+1
